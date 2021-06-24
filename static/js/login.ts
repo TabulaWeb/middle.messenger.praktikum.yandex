6d4115e75610881cb0362ce11eb5ad9cfc66880c
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-06-24 23:35:17 +0300

    component

--- static/js/login.ts
+++ static/js/login.ts
@@ -1,13 +1,21 @@
+const isCheckboxOrRadio = type => ['checkbox', 'radio'].includes(type);
 const {form} = document.forms as HTMLCollection
 
 function retrieveFormValue(event: { preventDefault: () => void; }){
   event.preventDefault()
 
-  const { login, password } = form;
+  const { elements } = form;
+  const values = {};
 
-  const values = {
-    login: login.value,
-    password: password.value
+  for (let i = 0; i < elements.length; i++){
+    const formElement = elements[i];
+    const {name} = formElement;
+
+    if (name){
+      const {value, type, checked} = formElement;
+
+      values[name] = isCheckboxOrRadio(type) ? checked : value;
+    }
   }
 
   console.log(values)
