b6b7f14218effb092119ec70e9db913941f3f14d
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-06-25 01:44:04 +0300

    validation

--- static/js/signin.ts
+++ static/js/signin.ts
@@ -1,33 +1,60 @@
-const btn = document.getElementById("btn") as HTMLButtonElement
+const inputMail = (<HTMLInputElement>document.querySelector("#email"))
+const inputLogin = (<HTMLInputElement>document.querySelector("#display_name"))
+const inputFirstName = (<HTMLInputElement>document.querySelector("#first_name"))
+const inputSecondName = (<HTMLInputElement>document.querySelector("#second_name"))
+const inputPhone = (<HTMLInputElement>document.querySelector("#phone"))
+const inputPassword = (<HTMLInputElement>document.querySelector("#password"))
+const inputPasswordTwo = (<HTMLInputElement>document.querySelector("#passwordTwo"))
+const textError = document.querySelector('.error_signin') as HTMLFormElement
 
-btn.onclick = () =>{
-    const inputMail = (<HTMLInputElement>document.querySelector("#email")).value
-    let inputLogin = (<HTMLInputElement>document.querySelector("#display_name")).value
-    let inputFirstName = (<HTMLInputElement>document.querySelector("#first_name")).value
-    let inputSecondName = (<HTMLInputElement>document.querySelector("#second_name")).value
-    let inputPhone = (<HTMLInputElement>document.querySelector("#phone")).value
-    let inputPassword = (<HTMLInputElement>document.querySelector("#password")).value
-    let inputPasswordTwo = (<HTMLInputElement>document.querySelector("#passwordTwo")).value
-    let textError = document.querySelector('.error_signin') as HTMLFormElement
-
-    let CreateUser = {
-        mail: inputMail,
-        login: inputLogin,
-        first_name: inputFirstName,
-        second_name: inputSecondName,
-        phone: inputPhone,
-        password: inputPassword,
-        password_two: inputPasswordTwo
-    }
-
-    console.log(CreateUser)
-    if(inputMail.length == 0 || inputLogin.length == 0 || inputFirstName.length == 0 || inputSecondName.length == 0 || inputPhone.length == 0){
+function errorSignin(){
+    if(inputMail.value.length == 0 || inputLogin.value.length == 0 || inputFirstName.value.length == 0 || inputSecondName.value.length == 0 || inputPhone.value.length == 0){
         return textError.textContent = 'есть не заполненное поле';
-    } else if(inputPasswordTwo.length < 6 && inputPassword.length < 6){
+    } else if (!inputMail.value.includes('@')){
+        return textError.textContent = 'введите правильный email';
+    } else if(inputPasswordTwo.value.length < 6 && inputPassword.value.length < 6){
         return textError.textContent = 'пароль менее 6 символов';
-    } else if(inputPassword.toLowerCase() != inputPasswordTwo.toLowerCase()){
+    } else if(inputPassword.value.toLowerCase() != inputPasswordTwo.value.toLowerCase()){
         return textError.textContent = 'пароль не совпадает';
     } else {
         return window.location.pathname = '/'
     }
 }
+
+inputMail.onblur = function(){
+  if (!inputMail.value.includes('@')) { // не email
+    return textError.textContent = 'введите правильный email';
+  }
+}
+
+inputMail.onfocus = function(){
+  if (!inputMail.value.includes('@')) { // не email
+    return textError.textContent = '';
+  }
+}
+
+inputPassword.onblur = function(){
+  if (inputPasswordTwo.value.length < 6 && inputPassword.value.length < 6) { // не email
+    return textError.textContent = 'пароль менее 6 символов';
+  }
+}
+
+inputPassword.onfocus = function(){
+  if (inputPasswordTwo.value.length < 6 && inputPassword.value.length < 6) { // не email
+    return textError.textContent = '';
+  }
+}
+
+inputPasswordTwo.onblur = function(){
+  if (inputPassword.value.toLowerCase() != inputPasswordTwo.value.toLowerCase()) { // не email
+    return textError.textContent = 'пароль не совпадает';
+  }
+}
+
+inputPasswordTwo.onfocus = function(){
+  if (inputPassword.value.toLowerCase() != inputPasswordTwo.value.toLowerCase()) { // не email
+    return textError.textContent = '';
+  }
+}
+
+
