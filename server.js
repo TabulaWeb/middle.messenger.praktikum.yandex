6dba52dc1db346830243838df073be9944e2264a
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-06-25 06:24:52 +0300

    server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'pug');
-app.set('views', path.resolve(__dirname, 'static'));
+app.set('views', path.resolve(__dirname, 'static/page'));
 
 app.use(express.static(path.resolve(__dirname, 'static')));
 
