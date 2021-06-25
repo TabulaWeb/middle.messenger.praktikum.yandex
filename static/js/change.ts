c09f861d3aff5211ca6771e5091e575c1b5330af
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-06-25 04:49:55 +0300

    change

--- static/js/change.ts
+++ static/js/change.ts
@@ -3,13 +3,13 @@ function fileUploader(elem: any): void {
     const outputElem = document.querySelector("#upload-message");
 
     inputElem.addEventListener("change", function () {
-      const fileList = inputElem.files[0];
+      const fileList = (<HTMLInputElement>inputElem).files[0];
       const uploadReader = new FileReader();
 
       outputElem.setAttribute("style", "display:block;")
       outputElem.innerHTML = fileList.name
       uploadReader.readAsDataURL(fileList);
-      if(document.getElementById("upload").files.length >= 1){
+      if((<HTMLInputElement>document.getElementById("upload")).files.length >= 1){
           document.querySelector('.fileUploader__button').setAttribute("style", "display:none;")
       }
     });
@@ -17,11 +17,11 @@ function fileUploader(elem: any): void {
 
 function changeFile(){
     let nameInputElem = document.querySelector("#upload");
-    let fileList = nameInputElem.files[0];
+    let fileList = (<HTMLInputElement>nameInputElem).files[0];
     let TextOutputElem = document.querySelector("#upload-message");
 
 
-    if(nameInputElem.files.length <= 0){
+    if((<HTMLInputElement>nameInputElem).files.length <= 0){
         return document.querySelector('.FileForm__error').textContent = "Нужно выбрать файл"
     } else {
         TextOutputElem.setAttribute("style", "display:none;")
